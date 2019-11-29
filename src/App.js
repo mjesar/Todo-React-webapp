b8c80a58509f649b29f853c54453682012a28f03
mjesar <mohammadalijaisar@gmail.com>
2019-11-29 10:10:51 +0500

    basic list component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { Button, Input } from "antd";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "work",
+      description: "Description",
+      status: true
+    };
+  }
+
+  render() {
+    const { title, description, status } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h1>{description}</h1>
+        <h1>{status? 'done':"uncomplete"}</h1>
+
+        <Button type="primary" icon="poweroff">
+          Click me!
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/todo-list/todo-list.jsx
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <P>TodoList</P>
+      </Fragment>
+    );
+  }
+}
