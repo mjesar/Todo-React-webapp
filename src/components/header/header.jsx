51a14a56fb7f8c3b8d5cfd101bef022c8f64202c
mjesar <mohammadalijaisar@gmail.com>
2019-12-01 16:06:34 +0500

    added header with logout button and titl

---
+++ src/components/header/header.jsx
@@ -0,0 +1,45 @@
+import React, { Component, Fragment } from "react";
+import { Layout, Row, Col, Menu, Dropdown, Icon } from "antd";
+import "./header.css";
+const { Header } = Layout;
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a rel="noopener noreferrer" href="#">
+        <Icon type="logout" theme="outlined" className="dropdown-icon" /> Logout
+      </a>
+    </Menu.Item>
+  </Menu>
+);
+class MainHeader extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header style={{ width: "100%" }}>
+          <div className={"userInfo"}>
+            <Row>
+              <div className="spanInfo">
+                <Col span={16}>
+                  <span className="spanClass">Todo App</span>
+                  <span className="spanClass" style={{ marginLeft: 10 }}></span>
+                </Col>
+                <Col span={8}>
+                  <Dropdown overlay={menu} placement="bottomCenter">
+                    <img
+                      style={{ float: "right", marginTop: 10 }}
+                      src="https://res.cloudinary.com/asadaziz/image/upload/v1561444483/dummyavatar_kb3aub.png"
+                      alt=""
+                      className="avatarImg"
+                    />
+                  </Dropdown>
+                </Col>
+              </div>
+            </Row>
+          </div>
+        </Header>
+      </Fragment>
+    );
+  }
+}
+export default MainHeader;
